3fa15d954611e8480e186c90ed051ce5aa2844e1
Dmitry Shuranov <dvshur@gmail.com>
2021-04-20 21:02:34 +0300

    mock sync repo with tracing

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,17 @@ use std::sync::{
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt::init();
+
     let mut handlers = vec![];
 
-    let heavy = Heavy::new();
+    let repo = repo::Sync::new();
 
     for i in 0..1000usize {
-        // let i = i.clone();
+        let repo = repo.clone();
 
-        let heavy = heavy.clone();
-
-        // handlers.push(tokio::task::spawn(async move { heavy.go(i) }));
-        handlers.push(tokio::task::spawn_blocking(move || heavy.go(i)));
+        handlers.push(tokio::task::spawn(async move { repo.fetch(i) }));
+        // handlers.push(tokio::task::spawn_blocking(move || repo.fetch(i)));
     }
 
     join_all(handlers).await;
@@ -30,35 +30,35 @@ async fn main() {
     );
 }
 
-#[derive(Clone)]
-struct Heavy {
-    concurrent_threads: Arc<AtomicUsize>,
-    // max_concurrent_threads: Arc<AtomicUsize>,
-}
+// #[derive(Clone)]
+// struct Heavy {
+//     concurrent_threads: Arc<AtomicUsize>,
+//     // max_concurrent_threads: Arc<AtomicUsize>,
+// }
 
-impl Heavy {
-    pub fn new() -> Self {
-        Self {
-            concurrent_threads: Arc::new(AtomicUsize::new(0)),
-            // max_concurrent_threads: Arc::new(AtomicUsize::new(0)),
-        }
-    }
+// impl Heavy {
+//     pub fn new() -> Self {
+//         Self {
+//             concurrent_threads: Arc::new(AtomicUsize::new(0)),
+//             // max_concurrent_threads: Arc::new(AtomicUsize::new(0)),
+//         }
+//     }
 
-    pub fn go(&self, thread_id: usize) {
-        let threads_before = self.concurrent_threads.fetch_add(1, Ordering::SeqCst);
+//     pub fn go(&self, thread_id: usize) {
+//         let threads_before = self.concurrent_threads.fetch_add(1, Ordering::SeqCst);
 
-        // self.max_concurrent_threads.fetch_update(set_order, fetch_order, mutf)
+//         // self.max_concurrent_threads.fetch_update(set_order, fetch_order, mutf)
 
-        // if threads_before >= self.max_concurrent_threads.load() {
-        //     // self.concurrent_threads.fetch_add(1, Ordering::SeqCst);
-        //     self.max_concurrent_threads
-        // }
+//         // if threads_before >= self.max_concurrent_threads.load() {
+//         //     // self.concurrent_threads.fetch_add(1, Ordering::SeqCst);
+//         //     self.max_concurrent_threads
+//         // }
 
-        eprintln!("START | {} | {}", thread_id, threads_before + 1);
+//         eprintln!("START | {} | {}", thread_id, threads_before + 1);
 
-        std::thread::sleep(std::time::Duration::from_secs(2));
+//         std::thread::sleep(std::time::Duration::from_secs(2));
 
-        let threads_before = self.concurrent_threads.fetch_sub(1, Ordering::SeqCst);
-        eprintln!("END | {} | {}", thread_id, threads_before + 1);
-    }
-}
+//         let threads_before = self.concurrent_threads.fetch_sub(1, Ordering::SeqCst);
+//         eprintln!("END | {} | {}", thread_id, threads_before + 1);
+//     }
+// }
--- src/repo.rs
+++ src/repo.rs
@@ -1,30 +1,51 @@
-use std::fmt::Debug;
-use tracing::instrument;
+use std::{
+    fmt::Debug,
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
+};
+use tracing::{instrument, trace};
 
 #[derive(Debug, Clone)]
-pub struct Sync {}
+pub struct Sync {
+    concurrent_threads: Arc<AtomicUsize>,
+}
 
 impl Sync {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            concurrent_threads: Arc::new(AtomicUsize::new(0)),
+        }
     }
 
     #[instrument]
     pub fn fetch<A: Debug>(&self, a: A) -> A {
+        let threads_before = self.concurrent_threads.fetch_add(1, Ordering::SeqCst);
+        let threads = threads_before + 1;
+
+        trace!(threads, "start");
+
+        std::thread::sleep(std::time::Duration::from_secs(2));
+
+        self.concurrent_threads.fetch_sub(1, Ordering::SeqCst);
+
+        trace!(threads, "end");
+
         a
     }
 }
 
-#[derive(Debug, Clone)]
-pub struct Async {}
+// #[derive(Debug, Clone)]
+// pub struct Async {}
 
-impl Async {
-    pub fn new() -> Self {
-        Self {}
-    }
+// impl Async {
+//     pub fn new() -> Self {
+//         Self {}
+//     }
 
-    #[instrument]
-    pub async fn fetch<A: Debug>(&self, a: A) -> A {
-        a
-    }
-}
+//     #[instrument]
+//     pub async fn fetch<A: Debug>(&self, a: A) -> A {
+//         a
+//     }
+// }
