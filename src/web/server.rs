a947e42c181979bd55bfd52a8562c3e5a31581ee
Dmitry Shuranov <dvshur@gmail.com>
2021-04-21 22:16:28 +0300

    change to sync impl for testing spans

--- src/web/server.rs
+++ src/web/server.rs
@@ -19,7 +19,8 @@ pub async fn start(repo: repo::Sync) {
         .and(warp::path::end())
         .and(with_repo.clone())
         .and_then(|r: repo::Sync| async move {
-            tokio::task::spawn_blocking(move || r.fetch(())).await;
+            r.fetch(());
+            // tokio::task::spawn_blocking(move || r.fetch(())).await;
             Result::<_, Infallible>::Ok(warp::reply())
         })
         .with(request_tracing);
